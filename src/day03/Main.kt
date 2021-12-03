05f2914bb5d85ac24997b4454fddf6a84d08f789
Maarten Ottenhof <m.ottenhof@moreapp.com>
2021-12-03 23:31:15 +0100

    Solution for day 3-2

--- src/day03/Main.kt
+++ src/day03/Main.kt
@@ -6,17 +6,27 @@ fun main() {
     val binary = File("src/day03/input").readLines()
 
     println(task1(binary))
+    println(task2(binary))
 }
 
 private fun task1(binary: List<String>): Int {
-    val gammaBinary = binary.fold(MutableList(binary[0].length) { 0 }) { acc, s ->
-        s.forEachIndexed { i, c -> if (c == '1') acc[i] = acc[i] + 1 }
-        acc
-    }
-        .map { if (it > binary.size / 2) '1' else '0' }
+    val gammaBinary = (0 until binary[0].length).map { binary.count { s -> s[it] == '1' } }
+        .joinToString(separator = "") { if (it > binary.size / 2) "1" else "0" }
+    val epsilonBinary = gammaBinary.map { if (it == '1') '0' else '1' }
         .joinToString(separator = "")
 
-    val epsilonBinary = gammaBinary.map { if (it == '1') '0' else '1' }.joinToString(separator = "")
-
     return gammaBinary.toInt(2) * epsilonBinary.toInt(2)
+}
+
+private fun task2(binary: List<String>) =
+    determineRating(binary) { b, i -> b.count { it[i] == '1' } >= b.size.toFloat() / 2 } *
+            determineRating(binary) { b, i -> b.count { it[i] == '0' } > b.size.toFloat() / 2 }
+
+private fun determineRating(binary: List<String>, index: Int = 0, b: (List<String>, Int) -> Boolean): Int {
+    val common = if (b(binary, index)) '1' else '0'
+    val filtered = binary.filter { it[index] == common }
+
+    return if (filtered.size == 1) {
+        filtered[0].toInt(2)
+    } else determineRating(filtered, index + 1, b)
 }
